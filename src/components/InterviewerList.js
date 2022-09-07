63bcdedd86a294fc23e81defc4d4aefd6fb15eb5
jes2806 <jes2806@gmail.com>
2022-09-07 15:05:54 -0600

    code refactoring for InterviewerList and IntervierwerListItem

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,23 +3,21 @@ import './InterviewerList.scss';
 import InterviewerListItem from './InterviewerListItem.js';
 
 export default function InterviewerList(props) {
-  const interviewersArray = props.interviewers.map((eachInterviewer) => {
-    console.log('console log:', eachInterviewer.name, props)
+  const interviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem 
-      key={eachInterviewer.id}
-      id={eachInterviewer.id}
-      name={eachInterviewer.name}
-      avatar={eachInterviewer.avatar}
-      selected={eachInterviewer.id === props.interviewer}
-      setInterviewer={props.setInterviewer}
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
     )
   })
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewersArray}</ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   )
 };
