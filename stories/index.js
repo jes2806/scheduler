2181fec064e0556095d0596e0c98dfae76e34735
jes2806 <jes2806@gmail.com>
2022-09-07 16:27:45 -0600

    add confirm empty header and show components

--- stories/index.js
+++ stories/index.js
@@ -3,7 +3,11 @@ import DayListItem from '../src/components/DayListItem.js';
 import DayList from '../src/components/DayList.js';
 import InterviewerListItem from '../src/components/InterviewerListItem';
 import InterviewerList from '../src/components/InterviewerList';
-import Appointment from "components/Appointment/index.js";
+import Appointment from '../src/components/Appointment/index.js';
+import Header from '../src/components/Appointment/Header';
+import Empty from '../src/components/Appointment/Empty';
+import Show from '../src/components/Appointment/Show';
+import Confirm from '../src/components/Appointment/Confirm';
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -139,4 +143,8 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12om" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
