a779093f809a786a05a0f1e66a5e2a8c5c834b3e
jes2806 <jes2806@gmail.com>
2022-09-07 16:51:37 -0600

    add Error and modify Show components

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import Empty from '../src/components/Appointment/Empty';
 import Show from '../src/components/Appointment/Show';
 import Confirm from '../src/components/Appointment/Confirm';
 import Status from '../src/components/Appointment/Status';
+import Error from '../src/components/Appointment/Error';
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -147,6 +148,11 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
-  .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Show", () => <Show
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    student="Lydia Miller-Jones"
+    interviewer={interviewers[0].name} />)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
   .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
